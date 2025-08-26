5656777d65426d498967e39065ee23833f694a88
Ruben van der Linde <ruben@conduction.nl>
2025-08-26 23:24:51 +0200

    Search optimalisatie

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { translate as t, translatePlural as n } from '@nextcloud/l10n'
 import AdminSettings from './views/settings/Settings.vue'
 import VueMarkdownEditor from '@kangc/v-md-editor'
 import '@kangc/v-md-editor/lib/style/base-editor.css'
@@ -6,6 +7,17 @@ import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
 import '@kangc/v-md-editor/lib/theme/style/github.css'
 import hljs from 'highlight.js'
 import enUS from '@kangc/v-md-editor/lib/lang/en-US.js'
+
+// Font Awesome setup for settings page
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+// Add all Font Awesome solid icons to the library
+library.add(fas)
+
+// Register FontAwesome component globally for settings
+Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 
 VueMarkdownEditor.use(githubTheme, {
 	Hljs: hljs,
