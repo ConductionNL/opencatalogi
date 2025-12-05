695473fe078b9993e92389f07cf9b3bb7788ffca
Barry Brands <barrybrands02@hotmail.com>
2025-12-05 16:34:41 +0100

    Added RobotsController

---
+++ lib/Controller/RobotsController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace OCA\OpenCatalogi\Controller;
+
+use OCA\OpenCatalogi\Service\SettingsService;
+use OCA\OpenCatalogi\Http\TextResponse;
+use OCP\AppFramework\Controller;
+use OCP\IRequest;
+use OCP\App\IAppManager;
+use OCP\IURLGenerator;
+use Psr\Container\ContainerInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use OCA\OpenCatalogi\Service\SitemapService;
+
+/**
+ * Class RobotsController
+ *
+ * Controller for handling publication-related operations in the OpenCatalogi app.
+ *
+ * @category  Controller
+ * @package   opencatalogi
+ * @author    Ruben van der Linde
+ * @copyright 2024
+ * @license   AGPL-3.0-or-later
+ * @version   1.0.0
+ * @link      https://github.com/opencatalogi/opencatalogi
+ */
+class RobotsController extends Controller
+{
+
+    
+    /**
+     * PublicationsController constructor.
+     *
+     * @param string             $appName            The name of the app
+     * @param IRequest           $request            The request object
+     * @param SettingsService    $settingsService    The settings service
+     * @param ContainerInterface $container          The container for dependency injection
+     * @param IAppManager        $appManager         The app manager
+     * @param IURLGenerator      $urlGenerator       The Nextcloud URL generator
+     */
+    public function __construct(
+        $appName,
+        IRequest $request,
+        private readonly SettingsService $settingsService,
+        private readonly ContainerInterface $container,
+        private readonly IAppManager $appManager,
+        private readonly IURLGenerator $urlGenerator,
+    ) {
+        parent::__construct($appName, $request);
+    }
+
+
+    /**
+     * Implements a preflighted CORS response for OPTIONS requests.
+     *
+     * @return \OCP\AppFramework\Http\Response The CORS response
+     *
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     * @PublicPage
+     */
+    public function index(): TextResponse
+    {
+        $settings = $this->settingsService->getSettings();
+
+        if (isset($settings['configuration']['catalog_register']) === false || isset($settings['configuration']['catalog_schema']) === false) {
+            return new TextResponse('Could net fetch settings', 500);
+        }
+
+        $catalogRegister = $settings['configuration']['catalog_register'];
+        $catalogSchema = $settings['configuration']['catalog_schema'];
+
+        $searchQuery['@self']['register'] = $catalogRegister;
+        $searchQuery['@self']['schema'] = $catalogSchema;
+
+        $catalogs = $this->getObjectService()->searchObjectsPaginated(
+            query: $searchQuery,
+            rbac: false,
+            multi: false,
+            published: false,
+            deleted: false
+        )['results'] ?? [];
+
+        $baseUrl = rtrim($this->urlGenerator->getBaseUrl(), '/');
+
+        $text = '';
+        foreach ($catalogs as $catalog) {
+            if (!$catalog->getSlug()) {
+                continue;
+            }
+
+            foreach (SitemapService::INFO_CAT as $categoryCode => $categoryName) {
+                $text .= "Sitemap: $baseUrl/apps/opencatalogi/api/{$catalog->getSlug()}/sitemaps/$categoryCode\n";
+            }
+        }
+
+        return new TextResponse($text);
+    }
+
+    /**
+     * Attempts to retrieve the OpenRegister service from the container.
+     *
+     * @return mixed|null The OpenRegister service if available, null otherwise.
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface
+     */
+    public function getObjectService(): ?\OCA\OpenRegister\Service\ObjectService
+    {
+        if (in_array(needle: 'openregister', haystack: $this->appManager->getInstalledApps()) === true) {
+            $this->objectService = $this->container->get('OCA\OpenRegister\Service\ObjectService');
+
+            return $this->objectService;
+        }
+
+        throw new \RuntimeException('OpenRegister service is not available.');
+
+    }//end getObjectService()
+
+}
