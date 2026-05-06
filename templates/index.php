de656e5522a8b40d75eb8986a874c32071db4b7c
Remko <remko@conduction.nl>
2026-05-06 10:50:33 +0200

    Added split chunks to index.php and admin.php

--- templates/index.php
+++ templates/index.php
@@ -3,6 +3,8 @@
 use OCP\Util;
 
 $appId = OCA\OpenCatalogi\AppInfo\Application::APP_ID;
+Util::addScript($appId, $appId . '-shared-vendor');
+Util::addScript($appId, $appId . '-shared-nc-vue');
 Util::addScript($appId, $appId . '-main');
 Util::addStyle($appId, 'main');
 ?>
