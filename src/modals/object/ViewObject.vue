e7a7d13178a5884bc7c80de2ca9e2263ba8ed0a1
Thijn <thijn@conduction.nl>
2026-05-15 14:41:41 +0200

    updated icon colors to right variant

--- src/modals/object/ViewObject.vue
+++ src/modals/object/ViewObject.vue
@@ -2106,11 +2106,11 @@ export default {
 }
 
 .error-icon {
-	color: var(--color-error);
+	color: var(--color-element-error);
 }
 
 .warning-icon {
-	color: var(--color-warning);
+	color: var(--color-element-warning);
 }
 
 .lock-icon {
@@ -2194,11 +2194,11 @@ export default {
 }
 
 .warningIcon {
-	color: var(--color-warning);
+	color: var(--color-element-warning);
 }
 
 .publishedIcon {
-	color: var(--color-success);
+	color: var(--color-element-success);
 }
 
 .tab-title {
@@ -2261,15 +2261,15 @@ export default {
 }
 
 .published-icon {
-	color: var(--color-success);
+	color: var(--color-element-success);
 }
 
 .draft-icon {
-	color: var(--color-warning);
+	color: var(--color-element-warning);
 }
 
 .depublished-icon {
-	color: var(--color-error);
+	color: var(--color-element-error);
 }
 
 /* Files info card */
